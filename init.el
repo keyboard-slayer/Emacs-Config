409e0a9cde9e210df808ad50f7e2e0ceb7efe33d
キーボードスレイヤー <dalcjor@libmail.eu>
2020-11-21 21:37:28 +0100

    Added a lot of things :sueur:

--- init.el
+++ init.el
@@ -1,19 +1,26 @@
-(setq inhibit-startup-message t)
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
 (tooltip-mode -1)
 (set-fringe-mode 10)
 (menu-bar-mode -1)
+(setq ring-bell-function 'ignore)
 
 (set-face-attribute 'default nil :font "Hack Nerd Font" :height 240)
 
-(load-theme 'tango-dark)
+(column-number-mode)
+(global-display-line-numbers-mode t)
+
+(dolist (mode '(org-mode-hook
+		term-mode-hook
+		shell-mode-hook
+		eshell-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode m))))
 
 (require 'package)
-
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
                          ("org" . "https://orgmode.org/elpa/")
                          ("elpa" . "https://elpa.gnu.org/packages/")))
+
 
 (package-initialize)
 (unless package-archive-contents
@@ -25,6 +32,13 @@
 
 (require 'use-package)
 (setq use-package-always-ensure t)
+
+(use-package evil
+  :config
+  (evil-mode 1))
+
+(use-package swiper
+  :ensure t)
 
 
 (use-package ivy
@@ -42,17 +56,98 @@
          :map ivy-reverse-i-search-map
          ("C-k" . ivy-previous-line)
          ("C-d" . ivy-reverse-i-search-kill))
+  :init (ivy-mode 1))
+
+(use-package counsel
+  :bind (("M-x" . counsel-M-x)
+         ("C-x b" . counsel-ibuffer)
+         ("C-x C-f" . counsel-find-file)
+         ("C-M-l" . counsel-imenu)
+         :map minibuffer-local-map
+         ("C-r" . 'counsel-minibuffer-history))
+  :custom
+  (counsel-linux-app-format-function #'counsel-linux-app-format-function-name-only)
   :config
-  (ivy-mode 1))
+  (setq ivy-initial-inputs-alist nil))
+
+(use-package doom-modeline
+  :init (doom-modeline-mode 1)
+  :custom-face
+  (mode-line ((t (:height 0.85))))
+  (mode-line-inactive ((t (:height 0.85))))
+  :custom
+  (doom-modeline-height 15)
+  (doom-modeline-bar-width 6)
+  (doom-modeline-minor-modes t)
+  (doom-modeline-window-width-limit fill-column)
+  (doom-modeline-project-detection 'projectile))
+
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+(use-package which-key
+  :init (which-key-mode)
+  :diminish wich-key-mode
+  :config
+  (setq which-key-idle-delay 0.3))
+
+(use-package ivy-rich
+  :init (ivy-rich-mode 1))
+
+(use-package helpful
+  :custom
+  (counsel-describe-function-function #'helpful-callable)
+  (counsel-describe-variable-function #'helpful-variable)
+  :bind
+  ([remap describe-function] . counsel-describe-function)
+  ([remap describe-command] . helpful-command)
+  ([remap describe-variable] . counsel-describe-variable)
+  ([remap describe-key] . helpful-key))
+
+(use-package doom-themes :defer t)
+(load-theme 'doom-palenight t)
+
+(use-package dashboard
+  :config
+  (dashboard-setup-startup-hook))
+
+(use-package projectile
+  :diminish projectile-mode
+  :config (projectile-mode)
+  :custom ((projectile-completion-system 'ivy))
+  :bind-keymap
+  ("C-c p" . projectile-command-map)
+  :init
+  (when (file-directory-p "~/Documents/Prog")
+    (setq projectile-project-search-path '("~/Documents/Prog")))
+  (setq projectile-switch-project-action #'projectile-dired))
+
+(use-package doom-modeline
+  :config (doom-modeline 1)
+  :custom-face
+  (mode-line ((t (:height 0.85))))
+  (mode-line-inactive ((t (:height 0.85))))
+  :custom
+  (doom-modeline-height 15)
+  (doom-modeline-bar-width 6)
+  (doom-modeline-minor-modes t)
+  (doom-modeline-window-width-limit fill-column)
+  (doom-modeline-project-detection 'projectile))
+
+(use-package counsel-projectile
+  :config (counsel-projectile-mode))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(ivy use-package)))
+ '(package-selected-packages
+   '(exwm ivy-rich which-key rainbow-delimiters rainbow-modeline doom-modeline counsel swiper evil ivy use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(mode-line ((t (:height 0.85))))
+ '(mode-line-inactive ((t (:height 0.85)))))
