62471f48ee28ab66eaaa4ea9215ebb4ef98509ab
キーボードスレイヤー <dalcjor@libmail.eu>
2021-04-07 14:22:50 +0200

    Babel is better and restarted from scratch

--- init.el
+++ init.el
@@ -1,177 +1,34 @@
-(scroll-bar-mode -1)
-(tool-bar-mode -1)
-(tooltip-mode -1)
-(set-fringe-mode 10)
-(menu-bar-mode -1)
-(setq ring-bell-function 'ignore)
+(let ((gc-cons-percentage .6)
+      (gc-cons-threshold most-positive-fixnum)
+      (read-process-output-max (* 1024 1024)))
 
-(set-face-attribute 'default nil :font "Hack Nerd Font" :height 240)
 
-(column-number-mode)
-(global-display-line-numbers-mode t)
+(setq-default inhibit-startup-echo-area-message (user-login-name))
 
-(dolist (mode '(org-mode-hook
-		term-mode-hook
-		shell-mode-hook
-		eshell-mode-hook))
-  (add-hook mode (lambda () (display-line-numbers-mode m))))
 
 (require 'package)
-(setq package-archives '(("melpa" . "https://melpa.org/packages/")
-                         ("org" . "https://orgmode.org/elpa/")
-                         ("elpa" . "https://elpa.gnu.org/packages/")))
-
-
+(setq-default
+   load-prefer-newer t
+   package-enable-at-startup nil)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 (package-initialize)
-(unless package-archive-contents
-  (package-refresh-contents))
 
-;; Initialize use-package on non-Linux platforms
+
 (unless (package-installed-p 'use-package)
-  (package-install 'use-package))
-
-(require 'use-package)
-(setq use-package-always-ensure t)
-
-(use-package evil
-  :init
-  (evil-mode 1))
-
-(use-package swiper
-  :ensure t)
-
-(use-package magit
-  :commands (magit-status magit-get-current-branch)
-  :custom
-  (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
-
-(use-package evil-magit
-  :after magit)
+(package-refresh-contents)
+(package-install 'use-package t))
+(setq-default
+   use-package-always-defer t
+   use-package-always-ensure t)
 
 
-(use-package erc
-  :commands erc
-  :config
-  (setq
-   erc-nick "keyboard-slayer"
-   erc-auto-query 'bury
-   erc-join-buffer 'burry
-   erc-rename-buffers t
-   erc-lurker-hide-list '("JOIN" "PART" "QUIT")
-   erc-track-exclude-types '("JOIN" "NICK" "QUIT" "MODE")
-   erc-track-enable-keybindings nil
-   erc-track-visibility nil 
-   erc-fill-column 80
-   erc-fill-function 'erc-fill-static
-   erc-fill-static-center 20
-   erc-autojoin-channels-alist '(("freenode.net" "#cpcshell" "#devse"))))
+(use-package org :ensure org-plus-contrib)
 
-     
-(use-package ivy
-  :diminish
-  :bind (("C-s" . swiper)
-         :map ivy-minibuffer-map
-         ("TAB" . ivy-alt-done)
-         ("C-l" . ivy-alt-done)
-         ("C-j" . ivy-next-line)
-         ("C-k" . ivy-previous-line)
-         :map ivy-switch-buffer-map
-         ("C-k" . ivy-previous-line)
-         ("C-l" . ivy-done)
-         ("C-d" . ivy-switch-buffer-kill)
-         :map ivy-reverse-i-search-map
-         ("C-k" . ivy-previous-line)
-         ("C-d" . ivy-reverse-i-search-kill))
-  :init (ivy-mode 1))
 
-(use-package counsel
-  :bind (("M-x" . counsel-M-x)
-         ("C-x b" . counsel-ibuffer)
-         ("C-x C-f" . counsel-find-file)
-         ("C-M-l" . counsel-imenu)
-         :map minibuffer-local-map
-         ("C-r" . 'counsel-minibuffer-history))
-  :custom
-  (counsel-linux-app-format-function #'counsel-linux-app-format-function-name-only)
-  :config
-  (setq ivy-initial-inputs-alist nil))
+(org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
+(garbage-collect))
 
-(use-package doom-modeline
-  :init (doom-modeline-mode 1)
-  :custom-face
-  (mode-line ((t (:height 0.85))))
-  (mode-line-inactive ((t (:height 0.85))))
-  :custom
-  (doom-modeline-height 15)
-  (doom-modeline-bar-width 6)
-  (doom-modeline-minor-modes t)
-  (doom-modeline-window-width-limit fill-column)
-  (doom-modeline-project-detection 'projectile))
-
-(use-package rainbow-delimiters
-  :hook (prog-mode . rainbow-delimiters-mode))
-
-(use-package which-key
-  :init (which-key-mode)
-  :diminish wich-key-mode
-  :config
-  (setq which-key-idle-delay 0.3))
-
-(use-package ivy-rich
-  :init (ivy-rich-mode 1))
-
-(use-package helpful
-  :custom
-  (counsel-describe-function-function #'helpful-callable)
-  (counsel-describe-variable-function #'helpful-variable)
-  :bind
-  ([remap describe-function] . counsel-describe-function)
-  ([remap describe-command] . helpful-command)
-  ([remap describe-variable] . counsel-describe-variable)
-  ([remap describe-key] . helpful-key))
-
-(use-package doom-themes :defer t)
-(load-theme 'doom-palenight t)
-
-(use-package dashboard
-  :config
-  (dashboard-setup-startup-hook))
-
-(use-package projectile
-  :diminish projectile-mode
-  :config (projectile-mode)
-  :custom ((projectile-completion-system 'ivy))
-  :bind-keymap
-  ("C-c p" . projectile-command-map)
-  :init
-  (when (file-directory-p "~/Documents/Prog")
-    (setq projectile-project-search-path '("~/Documents/Prog")))
-  (setq projectile-switch-project-action #'projectile-dired))
-
-(use-package doom-modeline
-  :config (doom-modeline 1)
-  :custom-face
-  (mode-line ((t (:height 0.85))))
-  (mode-line-inactive ((t (:height 0.85))))
-  :custom
-  (doom-modeline-height 15)
-  (doom-modeline-bar-width 6)
-  (doom-modeline-minor-modes t)
-  (doom-modeline-window-width-limit fill-column)
-  (doom-modeline-project-detection 'projectile))
-
-(use-package counsel-projectile
-  :config (counsel-projectile-mode))
-
-(use-package evil-org
-  :after org
-  :config
-  (add-hook 'org-mode-hook 'evil-org-mode)
-  (add-hook 'evil-org-mode-hook
-	    (lambda ()
-            (evil-org-set-key-theme)))
-  (require 'evil-org-agenda)
-  (evil-org-agenda-set-keys))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -179,11 +36,11 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(exwm ivy-rich which-key rainbow-delimiters rainbow-modeline doom-modeline counsel swiper evil ivy use-package)))
+   '(rainbow-delimiters counsel ivy-rich ivy use-package powerline-evil org-plus-contrib evil-org)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(mode-line ((t (:height 0.85))))
- '(mode-line-inactive ((t (:height 0.85)))))
+ )
+(put 'downcase-region 'disabled nil)
