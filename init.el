312bf72ea2320209c4c66fb8b7a90b6c5a313081
Keyboard Slayer <dalcjor@libmail.eu>
2021-07-10 12:56:54 +0200

    Everything from scratch

--- init.el
+++ init.el
@@ -1,51 +1,15 @@
-(let ((gc-cons-percentage .6)
-      (gc-cons-threshold most-positive-fixnum)
-      (read-process-output-max (* 1024 1024)))
-
-
-(setq-default inhibit-startup-echo-area-message (user-login-name))
-
-
-(require 'package)
-(setq-default
-   load-prefer-newer t
-   package-enable-at-startup nil)
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
-(package-initialize)
-
-
-(unless (package-installed-p 'use-package)
-(package-refresh-contents)
-(package-install 'use-package t))
-(setq-default
-   use-package-always-defer t
-   use-package-always-ensure t)
-
-
-(use-package org :ensure org-plus-contrib)
-
-
-(org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
-(garbage-collect))
-
-
+(org-babel-load-file
+ (expand-file-name "config.org" user-emacs-directory))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("8a379e7ac3a57e64de672dd744d4730b3bdb88ae328e8106f95cd81cbd44e0b6" "2035a16494e06636134de6d572ec47c30e26c3447eafeb6d3a9e8aee73732396" default))
- '(elfeed-feeds
-   '(("https://www.reddit.com/r/devse.rss" reddit devse)
-     ("https://www.reddit.com/r/osdev.rss" reddit prog)))
  '(package-selected-packages
-   '(rainbow-delimiters counsel ivy-rich ivy use-package powerline-evil org-plus-contrib evil-org)))
+   '(dired-hide-dotfiles all-the-icons-dired visual-fill-column visual-fill org-bullets magit makefile-executor which-key use-package ranger rainbow-delimiters projectile multiple-cursors lsp-ui ivy-rich helm general ewal-doom-themes evil-collection elfeed doom-modeline diminish counsel ayu-theme ample-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-(put 'downcase-region 'disabled nil)
