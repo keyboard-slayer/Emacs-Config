2a179597ae85bd1cd50d94381eee26d9eb39e4a2
Keyboard Slayer <dalcjor@libmail.eu>
2022-01-08 13:50:26 +0100

    Exwm !

--- init.el
+++ init.el
@@ -5,8 +5,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("0d01e1e300fcafa34ba35d5cf0a21b3b23bc4053d388e352ae6a901994597ab1" "234dbb732ef054b109a9e5ee5b499632c63cc24f7c2383a849815dacc1727cb6" "23c806e34594a583ea5bbf5adf9a964afe4f28b4467d28777bcba0d35aa0872e" "8d7b028e7b7843ae00498f68fad28f3c6258eda0650fe7e17bfb017d51d0e2a2" "6c531d6c3dbc344045af7829a3a20a09929e6c41d7a7278963f7d3215139f6a7" "1f1b545575c81b967879a5dddc878783e6ebcca764e4916a270f9474215289e5" "a82ab9f1308b4e10684815b08c9cac6b07d5ccb12491f44a942d845b406b0296" "3d47380bf5aa650e7b8e049e7ae54cdada54d0637e7bac39e4cc6afb44e8463b" "cbdf8c2e1b2b5c15b34ddb5063f1b21514c7169ff20e081d39cf57ffee89bc1e" "6c98bc9f39e8f8fd6da5b9c74a624cbb3782b4be8abae8fd84cbc43053d7c175" "a9a67b318b7417adbedaab02f05fa679973e9718d9d26075c6235b1f0db703c8" "028c226411a386abc7f7a0fba1a2ebfae5fe69e2a816f54898df41a6a3412bb5" "7a7b1d475b42c1a0b61f3b1d1225dd249ffa1abb1b7f726aec59ac7ca3bf4dae" default))
+ '(org-agenda-files nil nil nil "Customized with use-package org")
  '(package-selected-packages
-   '(dired-hide-dotfiles all-the-icons-dired visual-fill-column visual-fill org-bullets magit makefile-executor which-key use-package ranger rainbow-delimiters projectile multiple-cursors lsp-ui ivy-rich helm general ewal-doom-themes evil-collection elfeed doom-modeline diminish counsel ayu-theme ample-theme)))
+   '(dashboard desktop-environment dired-hide-dotfiles all-the-icons-dired visual-fill-column visual-fill org-bullets magit makefile-executor which-key use-package ranger rainbow-delimiters projectile multiple-cursors lsp-ui ivy-rich helm general ewal-doom-themes evil-collection elfeed doom-modeline diminish counsel ayu-theme ample-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
